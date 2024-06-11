098b016a044e9bebe15af3abf0833138dbb102f4
Prakhar Mishra <mishra.prakhar436@gmail.com>
2024-06-12 03:20:11 +0530

    added auth test and initial layout

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users.routes";
 import authRoutes from "./routes/auth.routes";
+import path from "path";
 
 mongoose.connect(process.env.MONGO_DB_URI as string); // cast it as a string so that it doesn't cause problem if the uri is undefined
 
@@ -20,6 +21,15 @@ app.use(
   }) // if any strange url tries to access our backend => if they got a hold of http cookie ( extremely hard), still wouldn't be able
   // to access our server bcz the origin or the url they are trying to access from is different
 ); // prevents certain requests from certain urls
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist"))); // go to the frontend dist folder which has
+// our compiled frontend static assets and serve
+// those static assets on the route of our url
+// on which our backend runs on
+
+// so now we dont have to run the front end seperately and all we have to do now is start the backend
+// and backend served our frontend
+// everything gets bundled up together, which is nice for our course
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
